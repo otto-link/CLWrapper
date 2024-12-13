2f5ae3c4e6eb5dd25a233563f8d7cb6fa03a6150
Otto Link <otto.link.bv@gmail.com>
2024-12-13 22:36:31 +0100

    Fix cmake files to avoid duplicate targets while integrating the library

--- external/macro-logger.cmake
+++ external/macro-logger.cmake
@@ -1,8 +1,7 @@
-project(macro-logger)
+project(clwrapper-macro-logger)
 
 set(MACRO_LOGGER_DIR ${CMAKE_CURRENT_SOURCE_DIR}/macro-logger/)
 
 add_library(${PROJECT_NAME} INTERFACE)
-add_library(clwrapper::${PROJECT_NAME} ALIAS ${PROJECT_NAME})
 
 target_include_directories(${PROJECT_NAME} INTERFACE ${MACRO_LOGGER_DIR})
